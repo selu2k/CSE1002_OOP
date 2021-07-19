ca270b386e1134f602d3b9def0b744585b906104
Selukash <selu2kk@gmail.com>
2021-07-20 01:35:34 +0530

    efef

--- c/tempCodeRunnerFile.c
+++ c/tempCodeRunnerFile.c
@@ -1,2 +1,35 @@
+# include<stdio.h>
+#include <stdlib.h>
 
-{
+int main(){
+    // int i=79;
+    // int *j;
+    // j= &i;
+    // int **k=&j;
+    // int arr[]={1,2,3,4};
+    // int *ptr=arr;
+    int * ptr2;
+    for(int i=0;i<3;i++){
+    scanf("%d",ptr2[i]);}
+    // printf("%d\n",&arr[0]);
+    // printf("%d\n",ptr);
+     printf("%d\n",ptr2[2]);
+    // printf("%d\n",arr);
+    // printf("%d\n",*(ptr+1));
+    // printf("%d\n",(ptr+1));
+    // printf("%d\n",&*(ptr+1));
+    // printf("%d\n",&arr[1]);
+    // int *p;
+    // // p=(int *)malloc(sizeof(int));
+    // // scanf("%d",p);
+    // printf("%u",*p);
+    /*printf("%u\n",*j);
+    printf("%u\n",j);
+    printf("%d\n",i);
+    printf("%u\n",&i);
+    printf("%u\n",&j);
+    printf("%u\n",**k);
+    printf("%u\n",*k);
+    printf("%u\n",k);*/
+    return 0;
+ }
